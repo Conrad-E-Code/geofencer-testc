a34f9f27866bddd3fa7e8dfaca126756d4f1bf43
Conrad-E-Code <112827066+Conrad-E-Code@users.noreply.github.com>
2023-04-20 21:16:24 -0600

    test

--- util/location.js
+++ util/location.js
@@ -1,5 +1,4 @@
 export function getMapPreview(lat, lng) {
-    const imagePreviewURL = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:blue%7C${lat},${lng}&key=${process.env.GOOGLE_API_KEY}`
-    console.log(imagePreviewURL)
+    const imagePreviewURL = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:blue%7C${lat},${lng}&key=${GOOGLE_API_KEY}`
     return imagePreviewURL
 }
